80735600085bdb68d5ce3738f0a9607562bd0c49
heyongchao <528195677@qq.com>
2023-10-28 17:37:14 +0800

    feat: 测试 lint-staged功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: 'off',
+    // quotes: 'off',
     'space-before-function-paren': 'off'
   }
 }
