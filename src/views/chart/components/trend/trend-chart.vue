cdcb3e94fe7045c4cc1d76f1be8c98456319efad
heyongchao <528195677@qq.com>
2024-04-03 10:59:40 +0800

    Message: 14 数据模块基础模块结构、累计收益-数据模块渲染

---
+++ src/views/chart/components/trend/trend-chart.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="">trend-chart</div>
+</template>
+
+<script setup>
+import { defineOptions } from 'vue'
+defineOptions({ name: 'trend-chart' })
+</script>
+
+<style lang="scss" scoped></style>
